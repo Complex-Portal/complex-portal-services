b68e3660ff645240fe19463de0f97dfe4576e939
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2024-07-25 15:43:44 +0100

    :sparkles: Uniprot Id mapping service

---
+++ uniplex-import/src/main/java/uk/ac/ebi/complex/service/model/UniProt.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.complex.service.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+public class UniProt {
+    public static final String UNIPROT_API = "https://rest.uniprot.org";
+
+    public static class IdMapping {
+        public static final String ID_MAPPING_URL = UNIPROT_API + "/idmapping";
+
+        public static class Submit {
+            public static String SUBMIT_URL = ID_MAPPING_URL + "/run";
+
+            @Jacksonized
+            @Builder
+            @Data
+            @JsonIgnoreProperties(ignoreUnknown = true)
+            public static class Result {
+                private String jobId;
+            }
+        }
+
+
+        public static class Poll {
+            public static String POLL_URL = ID_MAPPING_URL + "/status/";
+
+            @Jacksonized
+            @Builder
+            @Data
+            @JsonIgnoreProperties(ignoreUnknown = true)
+            public static class Result {
+                private Status jobStatus;
+
+                public enum Status {
+                    RUNNING,
+                    FINISHED,
+                    QUEUED
+                }
+            }
+        }
+
+
+        public static class Result {
+            public static String RESULT_URL = ID_MAPPING_URL + "/uniprotkb/results/stream/";
+        }
+    }
+}
