fc336a59521b5130a63657d0d5d13787918450ca
jmedinaebi <jmedina@ebi.ac.uk>
2024-10-15 14:56:18 +0100

    Use complex service to iterate through complexes

--- pdb-import/src/main/java/uk/ac/ebi/complex/service/reader/PdbAssembliesReader.java
+++ pdb-import/src/main/java/uk/ac/ebi/complex/service/reader/PdbAssembliesReader.java
@@ -8,20 +8,19 @@ import org.springframework.batch.item.ItemStream;
 import org.springframework.batch.item.ItemStreamException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import psidev.psi.mi.jami.model.Complex;
 import psidev.psi.mi.jami.utils.XrefUtils;
 import uk.ac.ebi.complex.service.config.FileConfiguration;
 import uk.ac.ebi.complex.service.model.ComplexWithAssemblies;
-import uk.ac.ebi.intact.jami.dao.IntactDao;
 import uk.ac.ebi.intact.jami.model.extension.IntactComplex;
+import uk.ac.ebi.intact.jami.service.ComplexService;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Log4j
 @Component
@@ -31,25 +30,32 @@ public class PdbAssembliesReader implements ItemReader<ComplexWithAssemblies>, I
     private static final String WWPDB_DB_MI = "MI:0805";
     private static final String WWPDB_DB_NAME = "wwpdb";
 
-    private final IntactDao intactDao;
+    private final ComplexService complexService;
     private final FileConfiguration fileConfiguration;
     private final PdbAssembliesFileReader pdbAssembliesFileReader;
 
-    private Iterator<String> complexAcIterator;
+    private Iterator<Complex> complexIterator;
     private Map<String, Set<String>> complexAndAssemblies;
 
     @Override
     public ComplexWithAssemblies read() {
-        while (complexAcIterator.hasNext()) {
-            String complexAc = complexAcIterator.next();
-            IntactComplex complex = intactDao.getComplexDao().getLatestComplexVersionByComplexAc(complexAc);
-            if (complexAndAssemblies.containsKey(complexAc)) {
-                return new ComplexWithAssemblies(complexAc, complexAndAssemblies.get(complexAc));
-            }
+        while (complexIterator.hasNext()) {
+            Complex complex = complexIterator.next();
+            IntactComplex intactComplex = (IntactComplex) complex;
+            try {
+                String complexAc = intactComplex.getComplexAc();
+                if (complexAndAssemblies.containsKey(complexAc)) {
+                    return new ComplexWithAssemblies(complexAc, complexAndAssemblies.get(complexAc));
+                }
 
-            if (!XrefUtils.collectAllXrefsHavingDatabase(complex.getIdentifiers(), WWPDB_DB_MI, WWPDB_DB_NAME).isEmpty() ||
-                    !XrefUtils.collectAllXrefsHavingDatabase(complex.getIdentifiers(), WWPDB_DB_MI, WWPDB_DB_NAME).isEmpty()) {
-                return new ComplexWithAssemblies(complexAc, new HashSet<>());
+                if (!XrefUtils.collectAllXrefsHavingDatabase(complex.getIdentifiers(), WWPDB_DB_MI, WWPDB_DB_NAME).isEmpty() ||
+                        !XrefUtils.collectAllXrefsHavingDatabase(complex.getIdentifiers(), WWPDB_DB_MI, WWPDB_DB_NAME).isEmpty()) {
+                    return new ComplexWithAssemblies(complexAc, new HashSet<>());
+                }
+            } catch (Exception e) {
+                log.error("--- DEBUG ---");
+                e.printStackTrace();
+                log.error("--- DEBUG ---");
             }
         }
         return null;
@@ -67,10 +73,7 @@ public class PdbAssembliesReader implements ItemReader<ComplexWithAssemblies>, I
             String complexId = complexAndAssemblies.keySet().iterator().next();
             log.info("DEBUG: First assembly: complex id = " + complexId + " , assemblies = " + String.join("|", complexAndAssemblies.get(complexId)));
 
-            List<String> complexAcs = intactDao.getComplexDao().getAll().stream().map(IntactComplex::getComplexAc).collect(Collectors.toList());
-
-            log.info("DEBUG: Fetch " + complexAcs.size() + " complexes from DB");
-            this.complexAcIterator = complexAcs.iterator();
+            this.complexIterator = complexService.iterateAll();
         } catch (IOException e) {
             throw new ItemStreamException("Input file could not be read: " + fileConfiguration.getInputFileName(), e);
         }
