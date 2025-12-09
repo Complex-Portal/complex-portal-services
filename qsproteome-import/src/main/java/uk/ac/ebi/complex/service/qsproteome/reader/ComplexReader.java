b857bb597e83d39827a56d5b6fed5722f22c11e3
jmedinaebi <jmedina@ebi.ac.uk>
2025-12-09 11:13:08 +0000

    Update complex reader to filter on complex statuses

--- qsproteome-import/src/main/java/uk/ac/ebi/complex/service/qsproteome/reader/ComplexReader.java
+++ qsproteome-import/src/main/java/uk/ac/ebi/complex/service/qsproteome/reader/ComplexReader.java
@@ -11,14 +11,23 @@ import org.springframework.util.Assert;
 import psidev.psi.mi.jami.model.Complex;
 import uk.ac.ebi.intact.jami.dao.IntactDao;
 import uk.ac.ebi.intact.jami.model.extension.IntactComplex;
+import uk.ac.ebi.intact.jami.model.lifecycle.LifeCycleStatus;
 import uk.ac.ebi.intact.jami.service.ComplexService;
 
 import java.util.Iterator;
+import java.util.Set;
 
 @Log4j
 @Component
 @RequiredArgsConstructor
 public class ComplexReader implements ItemReader<IntactComplex>, ItemStream {
+
+    private static final Set<LifeCycleStatus> STATUSES_TO_CONSIDER = Set.of(
+            LifeCycleStatus.CURATION_IN_PROGRESS,
+            LifeCycleStatus.READY_FOR_CHECKING,
+            LifeCycleStatus.ACCEPTED,
+            LifeCycleStatus.READY_FOR_RELEASE,
+            LifeCycleStatus.RELEASED);
 
     private final IntactDao intactDao;
     private final ComplexService complexService;
@@ -26,9 +35,12 @@ public class ComplexReader implements ItemReader<IntactComplex>, ItemStream {
 
     @Override
     public IntactComplex read() {
-        if (complexIterator.hasNext()) {
+        while (complexIterator.hasNext()) {
             Complex complex = complexIterator.next();
-            return (IntactComplex) intactDao.getEntityManager().merge(complex);
+            IntactComplex intactComplex = (IntactComplex) intactDao.getEntityManager().merge(complex);
+            if (STATUSES_TO_CONSIDER.contains(intactComplex.getStatus())) {
+                return intactComplex;
+            }
         }
         return null;
     }
